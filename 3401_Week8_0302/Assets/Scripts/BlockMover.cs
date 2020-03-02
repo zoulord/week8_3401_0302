eacf7cdb8f27eed57d746dfc639f90c5a6f2ac62
Zul Tinarbuko <zt421918@scc-mmlab-02.local>
2020-03-02 15:42:28 -0500

    Updated script move blocker

---
+++ 3401_Week8_0302/Assets/Scripts/BlockMover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMover : MonoBehaviour
+{
+    public Transform leftBoundary;
+    public Transform rightBoundary;
+
+    public float moveSpeed;
+
+    bool isMovingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (isMovingRight)
+        {
+            // move block right
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+            // if we're too far to the right, turn around
+            if (transform.position.x > rightBoundary.position.x)
+            {
+                isMovingRight = false;
+            }
+
+        }
+        else
+        {
+            // move block left
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+            if (transform.position.x < leftBoundary.position.x)
+            {
+                isMovingRight = true;
+            }
+        }
+    }
+}
